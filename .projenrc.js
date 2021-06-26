6d00fba5e07ceb804fb6f08e16141f386dac7831
Greg Bomkamp <bomkampg@gmail.com>
2021-06-26 03:09:25 -0400

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,13 @@
+const { TypeScriptAppProject } = require('projen');
+const project = new TypeScriptAppProject({
+  defaultReleaseBranch: 'main',
+  name: 'kaleidoscinema',
+
+  // deps: [],                          /* Runtime dependencies of this module. */
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // release: undefined,                /* Add release management to this project. */
+});
+project.synth();
